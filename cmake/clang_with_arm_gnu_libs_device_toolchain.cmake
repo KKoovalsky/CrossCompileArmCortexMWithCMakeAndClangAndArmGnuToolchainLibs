84ff813440776e7457320231cc0ff4038ea30304
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-05-02 18:09:22 +0200

    Enable basic compilable executable

--- cmake/clang_with_arm_gnu_libs_device_toolchain.cmake
+++ cmake/clang_with_arm_gnu_libs_device_toolchain.cmake
@@ -22,9 +22,30 @@ set(basic_architecture_flags
 
 set(basic_flags "${basic_architecture_flags} -fdata-sections -ffunction-sections")
 
-set(CMAKE_C_FLAGS_INIT "${basic_flags}")
+list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES ARM_GNU_TOOLCHAIN_PATH)
+
+include(${CMAKE_CURRENT_LIST_DIR}/arm_gnu_toolchain_utils.cmake)
+ArmGnu_GetCSystemIncludeFlags(${basic_architecture_flags} c_system_includes)
+ArmGnu_GetCxxSystemIncludeFlags(${basic_architecture_flags} cxx_system_includes)
+ArmGnu_GetStandardLibrariesDirectory(${basic_architecture_flags} standard_libraries_dir)
+ArmGnu_GetCompilerRuntimeLibrariesDirectory(${basic_architecture_flags} compiler_runtime_libraries_dir)
+
+include(${CMAKE_CURRENT_LIST_DIR}/llvm_toolchain_utils.cmake)
+Llvm_GetDummyLibunwindDirectory(libunwind_dir)
+
+string(CONCAT exe_linker_flags
+    " --rtlib=libgcc"
+    " --stdlib=libstdc++"
+    " -L${standard_libraries_dir}"
+    " -L${compiler_runtime_libraries_dir}"
+    " -L${libunwind_dir}"
+)
+
+set(CMAKE_C_FLAGS_INIT "${basic_flags} ${c_system_includes}")
 set(CMAKE_ASM_FLAGS_INIT  "${basic_architecture_flags}")
-set(CMAKE_CXX_FLAGS_INIT "${basic_flags}")
+set(CMAKE_CXX_FLAGS_INIT "${basic_flags} ${cxx_system_includes}")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${exe_linker_flags}")
 
 set(CMAKE_C_COMPILER_TARGET arm-none-eabi)
 set(CMAKE_CXX_COMPILER_TARGET arm-none-eabi)
--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -10,3 +10,15 @@ function(ProvideLlvm)
     set(LLVM_TOOLCHAIN_PATH "${LLVM_TOOLCHAIN_SOURCE_DIR}" CACHE PATH "Path to the LLVM toolchain") 
 endfunction()
 
+function(ProvideArmGnuToolchain)
+    include(FetchContent)
+    FetchContent_Declare(
+        ArmGnuToolchain
+        URL https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
+        URL_HASH MD5=2383e4eb4ea23f248d33adc70dc3227e
+    )
+    FetchContent_MakeAvailable(ArmGnuToolchain)
+    FetchContent_GetProperties(ArmGnuToolchain SOURCE_DIR ARM_GNU_TOOLCHAIN_SOURCE_DIR)
+    set(ARM_GNU_TOOLCHAIN_PATH "${ARM_GNU_TOOLCHAIN_SOURCE_DIR}" CACHE PATH "Path to the ARM GNU toolchain")
+endfunction()
+
