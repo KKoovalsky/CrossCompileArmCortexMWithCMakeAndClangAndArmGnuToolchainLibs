035fe058d33229b54120286b2f4ce813f0a0882e
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-05-03 20:34:03 +0200

    Create demo AddDeviceExecutable()

--- cmake/device_executable.cmake
+++ cmake/device_executable.cmake
@@ -15,7 +15,23 @@ endfunction()
 
 function(MakeDeviceExecutable target_name)
 
-    target_link_options(${target_name} PRIVATE -T${DEVICE_LINKER_SCRIPT})
+    target_link_libraries(${target_name} PRIVATE device)
+    target_sources(${target_name} PRIVATE $<TARGET_OBJECTS:device_sources>)
+
+endfunction()
+
+function(CreateDeviceLibrary)
+
+    set(demo_dir ${PROJECT_ROOT_DIR_FOR_DEVICE_EXECUTABLE_CMAKE}/demo)
+    set(linker_script ${demo_dir}/STM32L432KCUx_FLASH.ld)
+
+    add_library(device_sources OBJECT ${demo_dir}/startup_stm32l432xx.s)
+    add_library(device STATIC 
+        ${demo_dir}/system_stm32l4xx.c)
+
+    target_compile_definitions(device PRIVATE STM32L432xx)
+    target_include_directories(device PRIVATE demo/include)
+    target_link_options(device PUBLIC -T${linker_script})
 
 endfunction()
 
@@ -23,6 +39,6 @@ endfunction()
 # Main script
 ################################################################################
 
-if(NOT DEVICE_LINKER_SCRIPT)
-    message(FATAL_ERROR " DEVICE_LINKER_SCRIPT must be set to a path to a proper linker script.")
-endif()
+set(PROJECT_ROOT_DIR_FOR_DEVICE_EXECUTABLE_CMAKE ${CMAKE_CURRENT_LIST_DIR}/..)
+
+CreateDeviceLibrary()
