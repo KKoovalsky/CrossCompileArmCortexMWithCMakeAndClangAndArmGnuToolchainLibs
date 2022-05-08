7ca0d4a3b6819a64e5ce301ef57f3ea41fbcd3bb
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-05-08 17:56:05 +0200

    Download the toolchains under the .deps dir

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,24 +1,40 @@
 function(ProvideLlvm)
+
     include(FetchContent)
     FetchContent_Declare(
         Llvm
         URL https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
         URL_HASH SHA256=61582215dafafb7b576ea30cc136be92c877ba1f1c31ddbbd372d6d65622fef5
+        PREFIX ${DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE}
+        SUBBUILD_DIR ${DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE}/llvm-subbuild
+        SOURCE_DIR ${DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE}/llvm-src
+        BINARY_DIR ${DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE}/llvm-build
     )
     FetchContent_MakeAvailable(Llvm)
+
     FetchContent_GetProperties(Llvm SOURCE_DIR LLVM_TOOLCHAIN_SOURCE_DIR)
     set(LLVM_TOOLCHAIN_PATH "${LLVM_TOOLCHAIN_SOURCE_DIR}" CACHE PATH "Path to the LLVM toolchain") 
+
 endfunction()
 
 function(ProvideArmGnuToolchain)
+
     include(FetchContent)
     FetchContent_Declare(
         ArmGnuToolchain
         URL https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
         URL_HASH MD5=2383e4eb4ea23f248d33adc70dc3227e
+        PREFIX ${DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE}
+        SUBBUILD_DIR ${DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE}/armgnutoolchain-subbuild
+        SOURCE_DIR ${DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE}/armgnutoolchain-src
+        BINARY_DIR ${DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE}/armgnutoolchain-build
     )
     FetchContent_MakeAvailable(ArmGnuToolchain)
+
     FetchContent_GetProperties(ArmGnuToolchain SOURCE_DIR ARM_GNU_TOOLCHAIN_SOURCE_DIR)
     set(ARM_GNU_TOOLCHAIN_PATH "${ARM_GNU_TOOLCHAIN_SOURCE_DIR}" CACHE PATH "Path to the ARM GNU toolchain")
+
 endfunction()
 
+set(PROJECT_ROOT_FOR_DEPENDENCIES_CMAKE ${CMAKE_CURRENT_LIST_DIR}/..)
+set(DEPENDENCIES_DIR_FOR_DEPENDENCIES_CMAKE ${PROJECT_ROOT_FOR_DEPENDENCIES_CMAKE}/.deps)
